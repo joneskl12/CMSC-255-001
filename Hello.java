15a5bf3689d3aaab51f3c4d61b33bbc2a5141e01
joneskl12 <112023563+joneskl12@users.noreply.github.com>
2022-08-24 16:22:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************************
+Hello
+************************************************
+Kyle Jones
+ 8-24-2022
+CMSC-255-001
+************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+	System.out.println("Kyle Jones,");
+	System.out.println("Hello!");
+	System.out.println("Lab 01");
+	System.out.println("CMSC-255-001");
+	System.out.println();
+
+	}
+}
